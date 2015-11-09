33238e75a8bf04bb641e3b08a7e1a33e0998fa96
buck <sebastian.buck@uni-tuebingen.de>
2015-11-09 23:02:14 +0100

    approach object works

--- src/states/approach_object.h
+++ src/states/approach_object.h
@@ -25,7 +25,9 @@ public:
     void exitAction();
     void iteration();
 
+private:
     void position(const sbc15_msgs::ObjectConstPtr &object_odom);
+    void driveToPose(tf::Pose object_base_link, double d);
 
 private:
     ros::Subscriber sub_objects;
@@ -34,7 +36,7 @@ private:
     double velocity_;
 
     ros::Time start_time_;
-    tf::Pose start_pose_;
+    tf::Pose start_pose_odom_;
 
     int type;
     std::deque< sbc15_msgs::ObjectConstPtr > objects_;
@@ -44,6 +46,8 @@ private:
 
     double last_error_pos_;
     double last_error_ang_;
+
+    std::map<std::string, tf::Transform> frame_to_base_link;
 };
 
 #endif // APPROACH_OBJECT_H
